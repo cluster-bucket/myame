92cb766ce61253e0cb80137761f90286ad522ad2
anonymous <anonymous>
2016-06-27 08:52:21 -0700

    Initial creation of app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # myame
 Myame Yaml and Markdown Editor
+
+http://mherman.org/blog/2015/01/31/local-authentication-with-passport-and-express-4/#.V3E5izVCjbQ
+https://simplemde.com/
---
+++ routes/posts.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+// GET /posts
+router.get('/', function (req, res, next) {
+    res.render('posts/index');
+});
+
+// GET /posts/:id
+router.get('/:id', function (req, res, next) {
+    res.render('show', {});
+});
+
+// GET /posts/:id/edit
+router.get('/:id/edit', function (req, res, next) {
+    res.send('edit');
+});
+
+// DELETE /posts/:id
+router.delete('/:id', function (req, res, next) {
+    res.send('destroy');
+});
+
+// GET /posts/new
+router.get('/new', function (req, res, next) {
+    res.send('new');
+});
+
+// POST /posts
+router.post('/', function (req, res, next) {
+    res.send('create');
+});
+
+// PATCH/PUT /posts/:id
+router.put('/:id', function (req, res, next) {
+    res.send('update');
+});
+
+module.exports = router;
